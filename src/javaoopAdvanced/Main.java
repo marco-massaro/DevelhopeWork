80eeb9bea6e65e6f3611ca3c4a268b056c17fec4
Marco Massaro <marco.massaro01@gmail.com>
2023-03-03 15:56:43 +0100

    Add: Maps exercise

--- src/javaoopAdvanced/Main.java
+++ src/javaoopAdvanced/Main.java
@@ -4,13 +4,19 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Set<String> ingredientsList = new HashSet<>();
-        ingredientsList.add("Tomato");
-        ingredientsList.add("Lettuce");
-        ingredientsList.add("Salad dressing");
-
-        for (String ingredient : ingredientsList){
-            System.out.println(ingredient);
+        HashMap<String,Double> ourMealPrices = new HashMap<>();
+        ourMealPrices.put("Pizza", 10.0);
+        ourMealPrices.put("Pizza", 10.0);
+        ourMealPrices.put("Salad", 8.0);
+        ourMealPrices.keySet();
+        for (String mapkey : ourMealPrices.keySet()) {
+            System.out.println("Our current key is " + mapkey);
+        }
+        for (Double mapValue : ourMealPrices.values()) {
+            System.out.println("Our current value is " + mapValue);
+        }
+        for (Map.Entry<String, Double> entry : ourMealPrices.entrySet()) {
+            System.out.println("Our current key is " + entry.getKey() + " and our current value is " + entry.getValue());
         }
     }
 }

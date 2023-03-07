9f88720d5983e9f78df24bde4b10f3c4582ce5c3
Marco Massaro <marco.massaro01@gmail.com>
2023-03-07 14:18:51 +0100

    Add: Reading & Writing to a file

---
+++ src/javaoopAdvanced/exercises/_4/Exercises.java
@@ -0,0 +1,135 @@
+package javaadvanced._4;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Exercises {
+
+    private static final Path MY_PATH = Paths.get("file.txt");
+    private static final Path PATH_IN_A_FOLDER = Paths.get("mydirectory/file.txt");
+
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4();
+        exercise5();
+    }
+
+    // Use the MY_PATH constant
+
+    /**
+     * 1:
+     * Write a method that writes myString to a file
+     * writes to a file
+     */
+    private static void exercise1() {
+        System.out.println("Exercise 1: ");
+        String myString = """
+                The quick brown fox jumps over the lazy dog
+                """;
+
+        try {
+            Files.createFile(MY_PATH);
+
+            // Your code here
+            Files.writeString(MY_PATH, myString);
+
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * 2:
+     * Write a method that reads the file from exercise 1
+     * then prints it out
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2: ");
+        try {
+            // Your code here
+            System.out.println(Files.readString(MY_PATH));
+
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+            System.exit(0);
+        }
+
+
+    }
+
+    /**
+     * 3:
+     * Write a method that reads a file and print the number of lines in the file
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3: ");
+        // Write code here to read the file and return the number of lines "\n", string.split    //ASK MARCO WTF IS HAPPENING
+
+        try {
+            // Your code here
+            Files.readString(MY_PATH).split("\n");
+            for (int i = 0; i < MY_PATH.toFile().length(); i++) {
+                System.out.println(i);
+            }
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * 4:
+     * Write a method that reads a file and returns the number of words in the file
+     * <p>
+     * Then deletes the previous file with Files.delete() use inside the try block
+     */
+    private static void exercise4() {
+        System.out.println("\nExercise 4: ");
+        try {
+            // Your code here
+            String myString = Files.readString(MY_PATH);
+            StringTokenizer tokens = new StringTokenizer(myString);
+
+            int tokenCount = tokens.countTokens();
+            System.out.println(String.valueOf(tokenCount));
+
+            Files.delete(MY_PATH);
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * 5:
+     * Using the `myDirectoryPath` and Files.createDirectory, fix the code below
+     */
+    private static void exercise5() {
+        System.out.println("\nExercise 5: ");
+        Path myDirectoryPath = Path.of("mydirectory");
+
+        try {
+            // Your code
+            Files.createDirectory(myDirectoryPath);
+            Files.writeString(PATH_IN_A_FOLDER, "Why am I in a folder?");
+
+        } catch (Exception exception) {
+            System.err.println("There was an error!");
+            exception.printStackTrace();
+            System.exit(0);
+        }
+    }
+}
--- src/shared/Main.java
+++ src/shared/Main.java
@@ -1,31 +1,58 @@
 package shared;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class Main {
+    private static final Random RANDOM = new Random();
     public static void main(String[] args) {
-        Map<String, Meal> menu = new HashMap<>();
-        Meal beefBurger = new Meal(7.80, 500, Set.of("Bun", "Patty", "Sauce"));
-        Meal cheeseBurger = new Meal(7.80, 500, Set.of("Bun", "Patty", "Sauce", "Cheese single"));
+        Map<String, Meal> menu = generateMenu();
+        List<Meal> orders = generateOrders(menu);
+        String fileContent = convertOrdersToCsvContent(orders);
+        Path ordersCsvPath = Path.of("orders.csv");
+        writeToFile(fileContent,ordersCsvPath);
 
-        menu.put("Burger", beefBurger);
-        menu.put("Cheese Burger", cheeseBurger);
-        menu.put("Fries", new Meal(3.50,250, Set.of("Potato","Lots of oil")));
-        menu.put("Pizza", new Meal(10.00,700, Set.of("Dough","Cheese", "Tomato","Toppings")));
 
-        Set<Meal> burgers = new HashSet<>();
-        burgers.add(beefBurger);
-        burgers.add(cheeseBurger);
-
-        printOutMealPrices(burgers);
-        System.out.println("Then lists \n\n");
-        List<Meal> orders = new ArrayList<>();
-        orders.add(beefBurger);
-        orders.add(beefBurger);
-        printOutMealPrices(orders);
     }
-    private static void printOutMealPrices(Collection<Meal> ourMeals){
-        for (Meal ourMeal : ourMeals){
-            System.out.println(ourMeal);
+
+    private static void writeToFile(String fileContent, Path orderPath) {
+        try {
+            Files.writeString(orderPath , fileContent);
+        }catch (Exception e) {
+            System.err.println("There was an error");
+            System.exit(0);
         }
     }
+
+    private static String convertOrdersToCsvContent(List<Meal> orders) {
+        StringBuilder fileContentBuilder = new StringBuilder();
+        fileContentBuilder.append("Price, Weight in grams");
+
+        for (Meal order : orders) {
+            fileContentBuilder.append("\n").append(order.getPrice()).append(",")
+                    .append(order.getWeightInGrams()).append("\n");
+
+        }
+        return fileContentBuilder.toString();
+    }
+
+    private static List<Meal> generateOrders(Map<String, Meal> menu) {
+        List<Meal> orders = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            orders.add(menu.get(new ArrayList<>(menu.keySet()).get(RANDOM.nextInt(menu.size()))));
+        }
+        return orders;
+    }
+
+    private static Map<String, Meal> generateMenu() {
+        Map<String, Meal> menu = new HashMap<>();
+
+        menu.put("Burger", new Meal(7.80,500, Set.of("Bun","Patty","Sauce")));
+        menu.put("Cheese Burger", new Meal(7.80,500, Set.of("Bun","Patty","Sauce","Cheese single")));
+        menu.put("Fries", new Meal(3.50,250, Set.of("Potato","Lots of oil")));
+        menu.put("Pizza", new Meal(10.00,700, Set.of("Dough","Cheese", "Tomato","Toppings")));
+        return menu;
+    }
+
 }

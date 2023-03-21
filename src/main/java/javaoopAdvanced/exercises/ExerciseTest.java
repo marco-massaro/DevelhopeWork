119399900a9dbf81e36759ba744383424be9afbb
Marco Massaro <marco.massaro01@gmail.com>
2023-03-21 10:42:48 +0100

    Fix: exercise 3 with debugger

--- src/main/java/javaoopAdvanced/exercises/ExerciseTest.java
+++ src/main/java/javaoopAdvanced/exercises/ExerciseTest.java
@@ -1,7 +1,8 @@
-package javaoopAdvanced.exercises._13;
+package javaoopAdvanced.exercises;
+
 import org.junit.jupiter.api.Assertions;
 
-public class Exercises {
+public class ExerciseTest {
     public static void main(String[] args) {
         exercise1();
         exercise2();
@@ -10,14 +11,13 @@ public class Exercises {
 
     /**
      * 1:
-     *
+     * <p>
      * The following code is supposed to do a factorial of the number five, which looks like this: 5*4*3*2*1 = 120
-     *
+     * <p>
      * Find and fix the error using the debugger
-     *
      */
     private static void exercise1() {
-        System.out.println("\nExercise 1: Working Correctly! ");
+        System.out.println("\nExercise 1: ");
         int number = 5;
         int factorial = calculateFactorial(number);
         Assertions.assertEquals(120, factorial);
@@ -26,7 +26,7 @@ public class Exercises {
     private static int calculateFactorial(int number) {
         //Fix this code
         int result = 1;
-        for (int i = number; i >= 1; i --) { //Ask Marco how to use the debugger for this
+        for (int i = number; i >= 1; i -= 1) {
             result = result * i;
         }
         return result;
@@ -34,14 +34,13 @@ public class Exercises {
 
     /**
      * 2:
-     *
+     * <p>
      * The following code is supposed to reverse a string
-     *
+     * <p>
      * Find and fix the error using the debugger
-     *
      */
     private static void exercise2() {
-        System.out.println("\nExercise 2: Working Correctly! ");
+        System.out.println("\nExercise 2: ");
         String input = "hello";
         String reversedString = reverseString(input);
         Assertions.assertEquals("olleh", reversedString);
@@ -51,26 +50,25 @@ public class Exercises {
         //Fix this code
 
         String reversed = "";
-        for (int i = input.length() - 1; i >= 0; i--) {  // and it was i >= 1   //When I try to put i in the debugger, it says cannot find local variable i
-            reversed += input.charAt(i); //Here it was reversed += revers + input.charAt(i);
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
         }
         return reversed;
     }
 
     /**
      * 3:
-     *
+     * <p>
      * The following code is supposed to check if a number is positive or negative
      * but there is a semantic error in the if statement.
-     *
+     * <p>
      * Find and fix the error using the debugger
-     *
      */
     private static void exercise3() {
-        System.out.println("\nExercise 3: Working Correctly! ");
+        System.out.println("\nExercise 3: ");
         Assertions.assertFalse(isPositive(-5));
         Assertions.assertFalse(isPositive(-55));
-        Assertions.assertFalse(isPositive(0)); //I changed the Assertation to assert True instead of false. Is this the correct answer?
+        Assertions.assertFalse(isPositive(0));
         Assertions.assertTrue(isPositive(5));
         Assertions.assertTrue(isPositive(55));
         Assertions.assertTrue(isPositive(4567));
@@ -78,6 +76,6 @@ public class Exercises {
 
     private static boolean isPositive(int num) {
         //Fix this code
-        return num > 0; // What is there to even change here?
+        return num > 0;
     }
 }
